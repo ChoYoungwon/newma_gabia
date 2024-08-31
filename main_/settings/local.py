c59d92fbeec8b984c7bd338d6234a54e2f619572
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-08-31 18:21:26 +0900

    .
    .

--- main_/settings/local.py
+++ main_/settings/local.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '114.108.153.117', '.114.108.153.117', 'http://114.108.153.117',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '114.108.153.117']
 
 # SQLite를 로컬 개발 환경에서 사용
 DATABASES = {
