e8ff25550c2f4488ae5fb25ee5ef5e7a290954f4
ulbin67 <106312136+ulbin67@users.noreply.github.com>
2024-08-31 16:32:23 +0900

    .
    .

--- main_/settings/production.py
+++ main_/settings/production.py
@@ -11,17 +11,24 @@ ALLOWED_HOSTS = [
     '114.108.153.117'
 ]
 
-# PostgreSQL이나 MySQL을 배포 환경에서 사용
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'newmadb',
-        'USER': 'main',
-        'PASSWORD': 'qpqp1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'newmadb.sqlite3',
     }
 }
+
+# PostgreSQL이나 MySQL을 배포 환경에서 사용
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'newmadb',
+#         'USER': 'main',
+#         'PASSWORD': 'qpqp1234',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
