b303cd220b77b434d2106242375be84c9e5e3964
ChoYoungwon <74918493+Youngwonzone1@users.noreply.github.com>
2024-09-01 19:43:24 +0900

    2

--- main_/settings.py
+++ main_/settings.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 #디렉토리 경로 설정
 #서버 열 때 static 폴더를 잘 불러오기 위해 이 경로가 필요
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 #보안 키를 숨기기 위해 json에 저장
 #깃허브에 업로드 시, 깃 이그노어에 secrets.json 추가 후 푸쉬
