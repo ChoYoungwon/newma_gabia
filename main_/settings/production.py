77d89155f6e349fcbab7dc0783a353c32f1c4d87
ChoYoungwon <74918493+Youngwonzone1@users.noreply.github.com>
2024-09-01 22:23:08 +0900

    6

--- main_/settings/production.py
+++ main_/settings/production.py
@@ -26,36 +26,36 @@ DATABASES = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-# HTTPS 설정
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+# # HTTPS 설정
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_HSTS_SECONDS = 31536000
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
 
 # 로깅 설정
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'debug.log'),
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': os.path.join(BASE_DIR, 'debug.log'),
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     },
+# }
 
-# 세션의 기본 만료 시간 (초 단위)
-SESSION_COOKIE_AGE = 7200  # 2시간
-
-# 브라우저를 닫을 때 세션 만료 설정
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+# # 세션의 기본 만료 시간 (초 단위)
+# SESSION_COOKIE_AGE = 7200  # 2시간
+#
+# # 브라우저를 닫을 때 세션 만료 설정
+# SESSION_EXPIRE_AT_BROWSER_CLOSE = True
